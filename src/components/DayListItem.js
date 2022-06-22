43b41f33e8d7ed783fd1f5756229baab3ff1238f
Ik Sung Choi <ikchoi0@gmail.com>
2022-06-22 09:54:40 -0400

    Writing Appointment Tests

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,18 +4,24 @@ import "components/DayListItem.scss";
 export default function DayListItem(props) {
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
-    "day-list__item--full": !(props.spots > 0)
+    "day-list__item--full": !(props.spots > 0),
   });
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+    <li
+      className={dayClass}
+      onClick={() => props.setDay(props.name)}
+      data-testid="day"
+    >
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
 
-function formatSpots (spots) {
-  return (spots === 0 ? "no" : spots) + (spots === 1 ? " spot" : " spots") + " remaining";
+function formatSpots(spots) {
+  return (
+    (spots === 0 ? "no" : spots) +
+    (spots === 1 ? " spot" : " spots") +
+    " remaining"
+  );
 }
-
-
